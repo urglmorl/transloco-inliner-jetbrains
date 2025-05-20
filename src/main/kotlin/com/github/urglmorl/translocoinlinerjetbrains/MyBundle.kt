aeab96f271b3bf7412a3a5b1c7547832b7235f3d
GitHub Action <action@github.com>
2025-05-20 12:00:46 +0000

    Template cleanup

--- src/main/kotlin/com/github/urglmorl/translocoinlinerjetbrains/MyBundle.kt
+++ src/main/kotlin/com/github/urglmorl/translocoinlinerjetbrains/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.urglmorl.translocoinlinerjetbrains
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/urglmorl/translocoinlinerjetbrains/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/urglmorl/translocoinlinerjetbrains/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.urglmorl.translocoinlinerjetbrains.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.urglmorl.translocoinlinerjetbrains.MyBundle
+import com.github.urglmorl.translocoinlinerjetbrains.services.MyProjectService
 import javax.swing.JButton
 
 
